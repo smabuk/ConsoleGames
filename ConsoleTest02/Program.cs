e83040662c955811016c96912721ffb67c923666
smabuk <2011834+smabuk@users.noreply.github.com>
2023-01-08 21:47:00 +0000

    Display Q-Less dice rack

--- ConsoleTest02/Program.cs
+++ ConsoleTest02/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Smab.DiceAndTiles;
+
+QLessDice qlessDice = new();
+qlessDice.ShakeAndFillRack();
+
+Console.WriteLine("Q-Less dice rack");
+for (int i = 0; i < qlessDice.Rack.Count; i++) {
+	Console.Write($"┌───┐ ");
+}
+Console.WriteLine();
+foreach (LetterDie die in qlessDice.Rack) {
+	Console.Write($"│ {die.FaceValue.Display} │ ");
+}
+Console.WriteLine();
+for (int i = 0; i < qlessDice.Rack.Count; i++) {
+	Console.Write($"└───┘ ");
+}
+Console.WriteLine();
