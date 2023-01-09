7abee8cf5cc5fafc2aba1293cfc4e50b722882d0
smabuk <2011834+smabuk@users.noreply.github.com>
2023-01-09 17:43:21 +0000

    Display individual die

--- ConsoleTest02/Program.cs
+++ ConsoleTest02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Smab.DiceAndTiles;
+Console.Clear();
 
 QLessDice qlessDice = new();
 qlessDice.ShakeAndFillRack();
@@ -16,3 +17,25 @@ for (int i = 0; i < qlessDice.Rack.Count; i++) {
 	Console.Write($"└───┘ ");
 }
 Console.WriteLine();
+
+(int cursorCol, int cursorRow) = Console.GetCursorPosition();
+
+Console.Write("Die 0: ");
+DisplayDie(qlessDice.Rack[0], 12);
+
+Console.SetCursorPosition(20, cursorRow);
+Console.Write("Die 1: ");
+DisplayDie(qlessDice.Rack[1], 30);
+
+
+static void DisplayDie(LetterDie die, int? col = null, int? row = null, bool as3d = false) {
+	(int cursorCol, int cursorRow) = Console.GetCursorPosition();
+	col ??= cursorCol;
+	row ??= cursorRow;
+	Console.SetCursorPosition((int)col, (int)row);
+	Console.Write($"┌───┐");
+	Console.SetCursorPosition((int)col, (int)row + 1);
+	Console.Write($"│ {die.FaceValue.Display} │");
+	Console.SetCursorPosition((int)col, (int)row + 2);
+	Console.Write($"└───┘");
+}
