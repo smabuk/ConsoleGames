27fcd8c3c31962aa01e772e131cfbccf32923ec2
smabuk <2011834+smabuk@users.noreply.github.com>
2023-01-11 00:03:39 +0000

    QLess method

--- ConsoleTest02/Program.cs
+++ ConsoleTest02/Program.cs
@@ -1,27 +1,20 @@
 ﻿using Smab.DiceAndTiles;
 
-QLessDice qlessDice = new();
-qlessDice.ShakeAndFillRack();
-
 Console.Clear();
-Console.WriteLine("Q-Less");
 
-IEnumerable<LetterDie> rack = qlessDice.Rack;
+QLess();
 
-DisplayRack(rack, "Rack");
-DisplayRack(rack.Where(d =>  "AEIOU".Contains(d.FaceValue.Value!)), "Vowels", true);
-DisplayRack(rack.Where(d => !"AEIOU".Contains(d.FaceValue.Value!)), "Consonants", true);
+static void QLess() {
+	QLessDice qlessDice = new();
+	qlessDice.ShakeAndFillRack();
 
-static void DisplayDie(LetterDie die, int? col = null, int? row = null, bool as3d = false) {
-	(int cursorCol, int cursorRow) = Console.GetCursorPosition();
-	col ??= cursorCol;
-	row ??= cursorRow;
-	Console.SetCursorPosition((int)col, (int)row);
-	Console.Write($"┌───┐");
-	Console.SetCursorPosition((int)col, (int)row + 1);
-	Console.Write($"│ {die.FaceValue.Display} │");
-	Console.SetCursorPosition((int)col, (int)row + 2);
-	Console.Write($"└───┘");
+	Console.WriteLine("Q-Less");
+
+	IEnumerable<LetterDie> rack = qlessDice.Rack;
+
+	DisplayRack(rack, "Rack");
+	DisplayRack(rack.Where(d => "AEIOU".Contains(d.FaceValue.Value!)), "Vowels", true);
+	DisplayRack(rack.Where(d => !"AEIOU".Contains(d.FaceValue.Value!)), "Consonants", true);
 }
 
 static void DisplayRack(IEnumerable<LetterDie> dice, string name, bool sort = false) {
@@ -40,4 +33,16 @@ static void DisplayRack(IEnumerable<LetterDie> dice, string name, bool sort = fa
 	}
 
 	Console.WriteLine();
+}
+
+static void DisplayDie(LetterDie die, int? col = null, int? row = null, bool as3d = false) {
+	(int cursorCol, int cursorRow) = Console.GetCursorPosition();
+	col ??= cursorCol;
+	row ??= cursorRow;
+	Console.SetCursorPosition((int)col, (int)row);
+	Console.Write($"┌───┐");
+	Console.SetCursorPosition((int)col, (int)row + 1);
+	Console.Write($"│ {die.FaceValue.Display} │");
+	Console.SetCursorPosition((int)col, (int)row + 2);
+	Console.Write($"└───┘");
 }
