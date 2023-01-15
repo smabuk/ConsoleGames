6e46f9a0ecc7ff85ad3e991d63668548813035cb
smabuk <2011834+smabuk@users.noreply.github.com>
2023-01-15 19:06:24 +0000

    Refactoring

--- ConsoleGames/Boggle.cs
+++ ConsoleGames/Boggle.cs
@@ -3,19 +3,19 @@
 namespace ConsoleGames;
 
 public sealed class Boggle {
-	private const int DieDisplayWidth  = 5;
-	private const int DieDisplayHeight = 3;
-	private const int OneSecond = 1000;
+	private const int DieDisplayWidth  =    5;
+	private const int DieDisplayHeight =    3;
+	private const int OneSecond        = 1000;
 
 	private static readonly TimeSpan RedZone = new(0, 0, 10);
 
-	private readonly BoggleDice _boggleDice;
-	private List<Slot> _board = new();
-	private DictionaryOfWords? _dictionary;
-	private List<string> _words = new(); 
-	private long _timerStart;
-	private int _topRow = int.MinValue;
-	private int _bottomRow;
+	private readonly BoggleDice   _boggleDice;
+	private          List<Slot>   _board       = new();
+	private        List<string>   _words       = new(); 
+	private  DictionaryOfWords?   _dictionary;
+	private long                  _timerStart;
+	private int                   _bottomRow;
+	private int                   _topRow      = int.MinValue;
 
 	public Boggle(BoggleDice.BoggleType type, string? filename) {
 		Type = type;
@@ -28,17 +28,83 @@ public sealed class Boggle {
 			.Select((die, index) => new Slot(die.FaceValue.Display, index % _boggleDice.BoardSize, index / _boggleDice.BoardSize))
 			.ToList();
 
-		if (!string.IsNullOrWhiteSpace(filename)) {
+		if (string.IsNullOrWhiteSpace(filename) is false) {
 			_dictionary = new DictionaryOfWords(filename);
 		}
 	}
 
-	private record Slot(string Letter, int Col, int Row);
+	public  TimeSpan              GameLength { get; set; }  = new(0, 3, 0);
+	public  BoggleDice.BoggleType Type       { get; init; } = Classic4x4;
+	public  bool                  Verbose    { get; set; }  = false;
+	private TimeSpan              TimeRemaining => GameLength.Subtract(Stopwatch.GetElapsedTime(_timerStart));
 
-	public TimeSpan GameLength { get; set; } = new(0, 3, 0);
-	public BoggleDice.BoggleType Type { get; set; } = Classic4x4;
-	public bool Verbose { get; set; } = false;
-	
+	public void Play() {
+		string currentWord = "";
+		
+		Console.WriteLine();
+		(int _, _bottomRow) = Console.GetCursorPosition();
+		_timerStart = Stopwatch.GetTimestamp();
+		
+		while (TimeRemaining.Ticks > 0) {
+			DisplayBoard(currentWord);
+
+			ConsoleKey key = DisplayAndGetInput(_bottomRow, currentWord);
+			if (key == ConsoleKey.Escape) {
+				break;
+			} else if (key == ConsoleKey.Enter && currentWord.Length > 0) {
+				_words.Add(currentWord);
+				currentWord = "";
+			} else if (key == ConsoleKey.Backspace && currentWord.Length > 0) {
+				currentWord = currentWord[..^1];
+			} else if (key >= ConsoleKey.A && key <= ConsoleKey.Z) {
+				currentWord += key;
+			} 
+		}
+
+		FinalSummary(_bottomRow);
+	}
+
+	private (int score, string reason, ConsoleColor colour) CheckWord(string word) {
+		string reason = "";
+		ConsoleColor colour = Console.ForegroundColor;
+		List<Slot> validSlots = SearchBoard(word);
+		int score = 0;
+		if (validSlots.Count != 0) {
+			if (_dictionary is not null) {
+				if (_dictionary.IsWord(word)) {
+					score = ScoreWord(word, Type);
+				} else {
+					reason = "Not a valid word";
+					colour = ConsoleColor.Red;
+				}
+			} else {
+				score = ScoreWord(word, Type);
+			}
+		} else {
+			reason = "Unplayable";
+			colour = ConsoleColor.Red;
+		}
+
+		return (score, reason, colour);
+	}
+
+	private ConsoleKey DisplayAndGetInput(int row, string word) {
+		Console.SetCursorPosition(0, row);
+		Console.Write(new string(' ', Console.WindowWidth - 2));
+
+		Console.SetCursorPosition(0, row);
+		Console.Write($"Time remaining: ");
+		if (TimeRemaining < RedZone) {
+			Console.ForegroundColor = ConsoleColor.Red;
+		}
+		Console.Write($"{TimeRemaining:m':'ss}");
+
+		Console.ResetColor();
+		Console.Write($" Press <Esc> to exit... {word}");
+
+		return KeyReader.ReadKey(OneSecond);
+	}
+
 	public void DisplayBoard(string word = "") {
 		if (_topRow == int.MinValue) {
 			for (int i = 0; i < (_boggleDice.BoardSize * DieDisplayHeight); i++) {
@@ -54,84 +120,13 @@ public sealed class Boggle {
 		}
 
 		if (Verbose && word.Length > 0) {
-			List<Slot> validSlots = BoggleSearch(word);
+			List<Slot> validSlots = SearchBoard(word);
+			Console.ForegroundColor = ConsoleColor.Green;
 			foreach (Slot slot in validSlots) {
-				Console.ForegroundColor = ConsoleColor.Green;
 				DisplayDie(slot.Letter, slot.Col * DieDisplayWidth, _topRow + (slot.Row * DieDisplayHeight));
 			}
 			Console.ResetColor();
 		}
-	}
-
-	public void Play() {
-		_timerStart = Stopwatch.GetTimestamp();
-		Console.WriteLine();
-		(int _, _bottomRow) = Console.GetCursorPosition();
-		string currentWord = "";
-		while (TimeRemaining.Ticks > 0) {
-			DisplayBoard(currentWord);
-			ConsoleKey key = DisplayAndGetInput(_bottomRow, currentWord);
-			if (key == ConsoleKey.Escape) {
-				break;
-			} else if (key == ConsoleKey.Enter && currentWord.Length >= 1) {
-				_words.Add(currentWord);
-				currentWord = "";
-			} else if (key == ConsoleKey.Backspace && currentWord.Length >= 1) {
-				currentWord = currentWord[..^1];
-			} else if (key >= ConsoleKey.A && key <= ConsoleKey.Z) {
-				currentWord += key;
-			} 
-		}
-
-		FinalSummary(_bottomRow);
-	}
-
-	private ConsoleKey DisplayAndGetInput(int row, string word) {
-		Console.SetCursorPosition(0, row);
-		Console.Write(new string(' ', Console.WindowWidth - 2));
-		Console.SetCursorPosition(0, row);
-		Console.Write($"Time remaining: ");
-		if (TimeRemaining < RedZone) {
-			Console.ForegroundColor = ConsoleColor.Red;
-		}
-		Console.Write($"{TimeRemaining:m':'ss}");
-		Console.ResetColor();
-		Console.Write($" Press <Esc> to exit... {word}");
-		return KeyReader.ReadKey(OneSecond);
-	}
-
-	private void FinalSummary(int row) {
-		Console.SetCursorPosition(0, row);
-		Console.Write(new string(' ', Console.WindowWidth - 2));
-		Console.WriteLine();
-		Console.WriteLine("Score Word            Reason");
-		int totalScore = 0;
-		foreach (string word in _words.Order().Distinct()) {
-			string reason = "";
-			List<Slot> validSlots = BoggleSearch(word);
-			int score = 0;
-			if (validSlots.Count != 0) {
-				if (_dictionary is not null) {
-					if (_dictionary.IsWord(word)) {
-						score = ScoreWord(word, Type);
-						totalScore += score;
-					} else {
-						reason = "Not a valid word";
-						Console.ForegroundColor = ConsoleColor.Red;
-					}
-				} else {
-					score = ScoreWord(word, Type);
-					totalScore += score;
-				}
-			} else {
-				reason = "Unplayable";
-				Console.ForegroundColor = ConsoleColor.Red;
-			}
-			Console.WriteLine($"{score,4}  {word,-15} {reason}");
-			Console.ResetColor();
-		}
-		Console.WriteLine();
-		Console.WriteLine($"{totalScore,4}  Total Score ");
 	}
 
 	private static void DisplayDie(string letter, int? col = null, int? row = null) {
@@ -145,6 +140,24 @@ public sealed class Boggle {
 		Console.Write($"│ {letter, -2}│");
 		Console.SetCursorPosition((int)col, (int)row + 2);
 		Console.Write($"└───┘");
+	}
+
+	private void FinalSummary(int row) {
+		Console.SetCursorPosition(0, row);
+		Console.Write(new string(' ', Console.WindowWidth - 2));
+
+		Console.WriteLine();
+		Console.WriteLine("Score Word            Reason");
+		int totalScore = 0;
+		foreach (string word in _words.Order().Distinct()) {
+			(int score, string reason, ConsoleColor colour) = CheckWord(word);
+			totalScore += score;
+			Console.ForegroundColor = colour;
+			Console.WriteLine($"{score,4}  {word,-15} {reason}");
+			Console.ResetColor();
+		}
+		Console.WriteLine();
+		Console.WriteLine($"{totalScore,4}  Total Score ");
 	}
 
 	private static int ScoreWord(string word, BoggleDice.BoggleType boggleSetType) {
@@ -180,13 +193,13 @@ public sealed class Boggle {
 	/// </summary>
 	/// <param name="word"></param>
 	/// <returns>Returns the first list of slots that make up the word otherwise returns an empty List</returns>
-	private List<Slot> BoggleSearch(string word) {
+	private List<Slot> SearchBoard(string word) {
 		List<Slot> result = new();
 		int cols = _board.Max(x => x.Col) + 1;
 		int rows = _board.Max(x => x.Row) + 1;
 		bool[,] visited = new bool[rows, cols];
 		for (int i = 0; i < _board.Count; i++) {
-			if (BoggleDFS(word, 0, _board[i].Col, _board[i].Row, visited, result)) {
+			if (SearchBoardDFS(word, 0, _board[i].Col, _board[i].Row, visited, result)) {
 				return result;
 			}
 		}
@@ -203,7 +216,7 @@ public sealed class Boggle {
 	/// <param name="visited"></param>
 	/// <param name="result"></param>
 	/// <returns></returns>
-	private bool BoggleDFS(string word, int index, int col, int row, bool[,] visited, List<Slot> result) {
+	private bool SearchBoardDFS(string word, int index, int col, int row, bool[,] visited, List<Slot> result) {
 		if (index == word.Length) {
 			return true;
 		}
@@ -218,26 +231,20 @@ public sealed class Boggle {
 
 		Slot current = _board.First(x => x.Col == col && x.Row == row);
 		int newIndex = Math.Min(word.Length, index + current.Letter.Length);
-		if (current.Letter.Length == 1) {
-			if (current.Letter[0] != word[index]) {
-				return false;
-			}
-		} else {
-			if (current.Letter.ToUpperInvariant() != word[index..newIndex]) {
-				return false;
-			}
+		if (current.Letter.ToUpperInvariant() != word[index..newIndex]) {
+			return false;
 		}
 
 		result.Add(current);
 		visited[col, row] = true;
-		bool found = BoggleDFS(word, newIndex, col - 1, row - 1, visited, result) ||
-					 BoggleDFS(word, newIndex, col    , row - 1, visited, result) ||
-					 BoggleDFS(word, newIndex, col + 1, row - 1, visited, result) ||
-					 BoggleDFS(word, newIndex, col - 1, row    , visited, result) ||
-					 BoggleDFS(word, newIndex, col + 1, row    , visited, result) ||
-					 BoggleDFS(word, newIndex, col - 1, row + 1, visited, result) ||
-					 BoggleDFS(word, newIndex, col    , row + 1, visited, result) ||
-					 BoggleDFS(word, newIndex, col + 1, row + 1, visited, result);
+		bool found = SearchBoardDFS(word, newIndex, col - 1, row - 1, visited, result) ||
+					 SearchBoardDFS(word, newIndex, col    , row - 1, visited, result) ||
+					 SearchBoardDFS(word, newIndex, col + 1, row - 1, visited, result) ||
+					 SearchBoardDFS(word, newIndex, col - 1, row    , visited, result) ||
+					 SearchBoardDFS(word, newIndex, col + 1, row    , visited, result) ||
+					 SearchBoardDFS(word, newIndex, col - 1, row + 1, visited, result) ||
+					 SearchBoardDFS(word, newIndex, col    , row + 1, visited, result) ||
+					 SearchBoardDFS(word, newIndex, col + 1, row + 1, visited, result);
 		if (!found) {
 			result.Remove(current);
 		}
@@ -246,6 +253,5 @@ public sealed class Boggle {
 		return found;
 	}
 
-	private TimeSpan TimeRemaining => GameLength.Subtract(Stopwatch.GetElapsedTime(_timerStart));
-
+	private record Slot(string Letter, int Col, int Row);
 }
